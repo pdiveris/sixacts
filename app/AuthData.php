0ee15ce9071987866a2771746096d3d7136c982a
Petros Diveris <petros@urban-eye.com>
2019-06-24 11:56:01 +0100

    Add meta data in models.

--- app/AuthData.php
+++ app/AuthData.php
@@ -4,6 +4,42 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\AuthData
+ *
+ * @property int $id
+ * @property string $provider
+ * @property string $scheme
+ * @property string $meta
+ * @property string $theirId
+ * @property string $token
+ * @property string $tokenSecret
+ * @property string $nickname
+ * @property string $name
+ * @property string $email
+ * @property string $avatar
+ * @property string $user
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AuthData newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AuthData newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AuthData query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AuthData whereAvatar($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AuthData whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AuthData whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AuthData whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AuthData whereMeta($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AuthData whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AuthData whereNickname($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AuthData whereProvider($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AuthData whereScheme($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AuthData whereTheirId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AuthData whereToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AuthData whereTokenSecret($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AuthData whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AuthData whereUser($value)
+ * @mixin \Eloquent
+ */
 class AuthData extends Model
 {
     
--- app/Proposal.php
+++ app/Proposal.php
@@ -4,6 +4,30 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Proposal
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $body
+ * @property int $user_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property int $category_id
+ * @property string $colour
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Proposal newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Proposal newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Proposal query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Proposal whereBody($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Proposal whereCategoryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Proposal whereColour($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Proposal whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Proposal whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Proposal whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Proposal whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Proposal whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Proposal extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category'];
