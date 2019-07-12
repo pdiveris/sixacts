48cfdca4ea4bbd6547842c51a6c299085577666e
Petros Diveris <petros@urban-eye.com>
2019-07-12 12:30:26 +0100

    Satisfying phpcs

--- app/Http/Controllers/StaticController.php
+++ app/Http/Controllers/StaticController.php
@@ -8,7 +8,10 @@ use Laravel\Nova\Fields\Gravatar;
 class StaticController extends Controller
 {
     /**
-     * @param \Illuminate\Http\Request $request
+     * Temporary main proposal content result set
+     *
+     * @param \Illuminate\Http\Request $request Request
+     *
      * @return \Illuminate\Contracts\View\View
      */
     public function content(Request $request)
@@ -21,33 +24,48 @@ class StaticController extends Controller
     }
     
     /**
+     * Render the home view
+     *
      * @return \Illuminate\Contracts\View\View
      */
     public function home()
     {
         $proposals = \App\Proposal::all();
         $categories = \App\Category::all();
-        return view('static.welcome', ['proposals'=>$proposals, 'categories'=>$categories]);
+        return view(
+            'static.welcome',
+            ['proposals'=>$proposals, 'categories'=>$categories]
+        );
     }
-
+    
+    /**
+     * Compose the link to the author
+     *
+     * @param \App\Proposal $proposal proposal
+     *
+     * @return string
+     */
     public static function authorLink(\App\Proposal $proposal)
     {
-        if (null !== $proposal->user->display_name && '' !== $proposal->user->display_name) {
+        $displayName = $proposal->user->display_name;
+        if (null !== $displayName && '' !== $displayName) {
             return '@'.$proposal->user->display_name;
         }
-        $names = explode(' ',$proposal->user->name);
-        if (count($names) >= 2 ) {
-            return '@'.strtolower(substr($names[0],0,1)).strtolower(substr($names[1],0));
+        $names = explode(' ', $proposal->user->name);
+        if (count($names) >= 2) {
+            return '@'.
+                strtolower(substr($names[0], 0, 1)).
+                strtolower(substr($names[1], 0));
         } else {
             return '@'.strtolower($proposal->user->name);
         }
-        
     }
     
     /**
      * Try local avatar, social avatar, gravatar
      *
-     * @param \App\User $user
+     * @param \App\User $user user
+     *
      * @return string
      */
     public static function makeAvatar(\App\User $user): string
@@ -65,40 +83,55 @@ class StaticController extends Controller
      * Get either a Gravatar URL or complete image tag for a specified email address.
      *
      * @param string $email The email address
-     * @param int $s Size in pixels, defaults to 80px [ 1 - 2048 ]
-     * @param string $d Default imageset to use [ 404 | mp | identicon | monsterid | wavatar ]
-     * @param string $r Maximum rating (inclusive) [ g | pg | r | x ]
-     * @param bool $img True to return a complete IMG tag False for just the URL
-     * @param array $atts Optional, additional key/value attributes to include in the IMG tag
+     * @param int    $s     Size in pixels, defaults to 80px [ 1 - 2048 ]
+     * @param string $d     Default imageset to use
+     * @param string $r     Maximum rating (inclusive) [ g | pg | r | x ]
+     * @param bool   $img   True to return a complete IMG tag False for just the URL
+     * @param array  $atts  Optional, additional key/value attrs to include
+     *
      * @return string String containing either just a URL or a complete image tag
      *
      * @see https://en.gravatar.com/site/implement/images/php/
      */
-    public static function makeGravatar(string $email, $s = 80, $d = 'mp', $r = 'g', $img = false,  $atts = []): string
-    {
+    public static function makeGravatar(
+        string $email,
+        $s=80,
+        $d = 'mp',
+        $r = 'g',
+        $img = false,
+        $atts = []
+    ): string {
         $url = 'https://www.gravatar.com/avatar/';
-        $url .= md5( strtolower( trim( $email ) ) );
+        $url .= md5(strtolower(trim($email)));
         $url .= "?s=$s&d=$d&r=$r";
-        if ( $img ) {
+        if ($img) {
             $url = '<img src="' . $url . '"';
-            foreach ( $atts as $key => $val )
+            foreach ($atts as $key => $val) {
                 $url .= ' ' . $key . '="' . $val . '"';
+            }
             $url .= ' />';
         }
         return $url;
     }
     
     /**
+     * Return forum view
+     *
      * @return \Illuminate\Contracts\View\View
      */
     public function forum()
     {
         $proposals = \App\Proposal::all();
         $categories = \App\Category::all();
-        return view('forum.home', ['proposals'=>$proposals, 'categories'=>$categories]);
+        return view(
+            'forum.home',
+            ['proposals'=>$proposals, 'categories'=>$categories]
+        );
     }
     
     /**
+     * Return siginin view
+     *
      * @return \Illuminate\Contracts\View\View
      */
     public function signIn()
@@ -106,6 +139,11 @@ class StaticController extends Controller
         return \View::make('auth.signin', []);
     }
     
+    /**
+     * Return view User registration
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
     public function userRegistration()
     {
         return \View::make('static.registration', []);
