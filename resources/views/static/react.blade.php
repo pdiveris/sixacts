ffb10e51f2d40c0d4e055092f4bb9cda94a14f43
Petros Diveris <petros@urban-eye.com>
2019-07-18 17:07:40 +0100

    A with the previous commit, increased size of column 1 in sidebar, thus eliminating the need for a gap column (as originally requested by the ticket.)
    Closed #14.

--- resources/views/static/react.blade.php
+++ resources/views/static/react.blade.php
@@ -1,13 +1,11 @@
 @extends('layouts.forum')
 @section('content')
     <div class="columns">
-        <div class="column is-3 u-mright-20">
-            @include('partials.sidebar')
-        </div>
-        <div class="column is-1">
+        <div class="column is-1 hide-when-you-know">
+
         </div>
         <div class="column is-8">
-            <div id="proposals"></div>
+            <div id="tablets"></div>
         </div>
     </div>
 @stop
