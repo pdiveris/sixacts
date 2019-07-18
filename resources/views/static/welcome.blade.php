a5fbd5a08896e491b876af4f18c7506ff8762496
Petros Diveris <petros@urban-eye.com>
2019-07-18 16:51:09 +0100

    Replace the sidebar [cats column, gap column, main column] layout with [cats column, main column] where the cats column gets the space it needs, not a gap after it (badly) ad hoc when space's needed.

--- resources/views/static/welcome.blade.php
+++ resources/views/static/welcome.blade.php
@@ -1,13 +1,10 @@
 @extends('layouts.forum')
 @section('content')
     <div class="columns">
-        <div class="column is-3 u-mright-20">
+        <div class="column is-4 u-mright-20">
             @include('partials.sidebar')
         </div>
-        <div class="column is-1 hide-when-you-know">
-
-        </div>
-        <div class="column is-8">
+        <div class="column">
             <div id="proposals"></div>
         </div>
     </div>
