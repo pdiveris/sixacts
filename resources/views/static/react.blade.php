6fdbc0460f42e4a8181be928e5a68dd097721631
Petros Diveris <petros@urban-eye.com>
2019-07-18 03:19:56 +0100

    Introduced code in templates (a simple column) to give breathing space to categories, the aim being to be readable in all configurations. At the sam time, a media query was added as to make show them only when the area is crowded, i.e. below 1200 pixels width (media query.)

--- resources/views/static/react.blade.php
+++ resources/views/static/react.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.forum')
 @section('content')
     <div class="columns">
-        <div class="column is-3">
+        <div class="column is-3 u-mright-20">
             @include('partials.sidebar')
         </div>
-        <div class="column is-9">
+        <div class="column is-1">
+        </div>
+        <div class="column is-8">
             <div id="proposals"></div>
         </div>
     </div>
--- resources/views/static/welcome.blade.php
+++ resources/views/static/welcome.blade.php
@@ -4,8 +4,10 @@
         <div class="column is-3 u-mright-20">
             @include('partials.sidebar')
         </div>
-        <div class="column is-9">
-
+        <div class="column is-1 hide-when-you-know">
+        </div>
+        <div class="column is-8">
+            <div id="proposals"></div>
         </div>
     </div>
 @stop
