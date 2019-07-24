8a611f08027c1db208592fb2da74f29261eec74a
Petros Diveris <petros@urban-eye.com>
2019-07-24 21:12:32 +0100

    Added ''Categories Explained' and '9 Rules for the 6 Acts' Closes #29.
    Part of the wider re-arrangement and discussion, see #28.

---
+++ resources/views/static/categories.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.forum')
+@section('content')
+    <div class="columns">
+        <div class="column is-9">
+            <div class="box content">
+                <h1 class="title">The Six Reform Categories Explained</h1>
+                <p>
+                    <strong>
+                    There are mutiple issues concerning the health of democratic systems, all of which
+                    need to be addressed. To achieve a final document that covers the majority of these,
+                    we have spread the scope for policy proposals into the following broad categories...
+                    </strong>
+                </p>
+                <h2 class="title is-4">1. Voting and Voting Systems</h2>
+                <p>
+                    Policy proposals that address the way elections are conducted, such as who votes, how
+                    voting happens, when voting happens, and how voting results are recognized and applied.
+                </p>
+                <h2 class="title is-4">2. Government Structures and Conduct of Elected Representatives</h2>
+                <p>
+                    How elected representatives organise themselves, interact, debate issues, make decisions
+                    and the rules for re-election. Transparency, discosure of interests and the means by which
+                    those elected are held to account.
+                </p>
+                <h2 class="title is-4">3. Ensuring the Accuracy of Political Information</h2>
+                <p>
+                    New legislation aimed at preventing or curbing the spread of misinformation that impacts
+                    political choices, and at raising the quality of information used to justify policy making.
+                    This should include addressing the influence and integrity of both old and new media,
+                    e.g. limiting monopolistic control or creating the right to challenge deliberately
+                    misleading claims.
+                </p>
+                <h2 class="title is-4">Economic and Financial Influences</h2>
+                <p>
+                    The moderating or curtailing of external economic influences which interfere with or
+                    unduly influence democratic structures and processes. Increasing the democratisation of
+                    businesses, economic structures, economic policy-making, monetary systems, and the
+                    banking / finance industries.
+                </p>
+                <h2 class="title is-4">5. Citizen Participation</h2>
+                <p>
+                    Policies to spread and increase the effectiveness of community led, non-governmental
+                    democratic routes of influence.
+                </p>
+                <h2 class="title is-4">6. Other</h2>
+                <p>
+                    Any proposals outside the scope of the other categories.
+                </p>
+
+            </div>
+        </div>
+    </div>
+@stop
---
+++ resources/views/static/nine_rules.blade.php
@@ -0,0 +1,77 @@
+@extends('layouts.forum')
+@section('content')
+    <div class="columns">
+        <div class="column is-9">
+            <div class="box content">
+                <h1 class="title">9 Rules for the 6 Acts </h1>
+                <h2 class="title is-4">The suggested proposal MUST:</h2>
+                <ul>
+                    <li>
+                        <p>
+                            Directly propose solutions to problems within our current democratic systems
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Be relevant to one of the following 6 categories and be submitted into the
+                            correct category
+                            <ol>
+                                <li>
+                                    <strong>Voting and Voting Systems</strong>
+                                </li>
+                                <li>
+                                    <strong>Government Structures and Conduct of Elected Representatives</strong>
+                                </li>
+                                <li>
+                                    <strong>Ensuring the Accuracy of Political Information</strong>
+                                </li>
+                                <li>
+                                    <strong>Economic and Financial Influences</strong>
+                                </li>
+                                <li>
+                                    <strong>Citizen Participation</strong>
+                                </li>
+                                <li>
+                                    <strong>Other</strong>
+                                </li>
+                            </ol>
+                        </p>
+                    </li>
+                    <li>
+                        Be no more than 100 words in length
+                    </li>
+                    <li>
+                        Not contain any obscene or abusive language and must respect the spirit and
+                        dignity of the Six Acts project
+                    </li>
+                    <li>
+                        Not duplicate any previously submitted proposal
+                    </li>
+                    <li>
+                        Not be used as a means of protesting against, abusing or criticising other
+                        proposals, individuals or organisations
+                    </li>
+                    <li>
+                        Serve to advance the effectiveness of the democratic system in general and not
+                        the narrow, partisan interests of any particular person, group or party
+                    </li>
+                    <li>
+                        Be a serious, realistically achievable proposal and not simply a frivolous
+                        expression of frustration or wishful thinking
+                    </li>
+                    <li>
+                        Be concise, cogent and to the point
+                    </li>
+                </ul>
+            </div>
+            <div class="content">
+                <i>
+                The Six Acts Poll will be moderated. Our team of moderators will have the power to delete any
+                submissions which do not fulfil the above requirements. While attempts will be made to contact the
+                user in cases where a genuine error has occurred, moderators are not obliged to do so and
+                unacceptable submissions may be removed without warning.
+                </i>
+            </div>
+        </div>
+    </div>
+@stop
