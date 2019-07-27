43a94d7cdd9f3496ab25fd6c8aacaf025d29baa9
Petros Diveris <petros@urban-eye.com>
2019-07-27 23:11:08 +0100

    Changing http to https does the job. Closed #41.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -95,7 +95,7 @@
         </script>
         <!-- End Matomo Code -->
         <!-- the sixacts -->
-        <script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+        <script src="https://www.google-analytics.com/urchin.js" type="text/javascript">
         </script>
         <script type="text/javascript">
             _uacct = "UA-143185860-1";
