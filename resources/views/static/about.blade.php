67d87594c2dc005592fd5de74af57a0bd7b51972
Petros Diveris <petros@urban-eye.com>
2019-07-30 17:04:05 +0100

    Updated contents of about page as per document attached to ticket #47 and so closed #47.

--- resources/views/static/about.blade.php
+++ resources/views/static/about.blade.php
@@ -1,29 +1,107 @@
 @extends('layouts.forum')
 @section('content')
     <div class="columns">
-        {{--
-                <div class="column is-3">
-                    @include('partials.sidebar')
-                </div>
-        --}}
-        <div class="column is-9">
-            <div class="box content" style="min-height: 400px;">
-                <h1>Six Acts</h1>
-                <h2 class="title is-6">
+        <div class="column is-12">
+            <div class="box content" >
+                <h1 class="is-2">The Project</h1>
+                <h2 class="is-3">The Launch</h2>
+                <p>
+                    The Six Acts online poll will <i>go live to the public at 4pm on the 17th August 2019.</i>
+                </p>
+                <p>
+                    To mark the occasion and to commemorate the 200th anniversary of the Peterloo Massacre, the
+                    Peterloo Memorial Campaign and the Six Acts team will be throwing a public 'Illegal PICNIC'
+                    launch party on the site of the massacre – <strong>Manchester Central forecourt, Windmill Street,
+                    M2 3GX on the 17th August, from 3 – 5:30pm.</strong>
+                </p>
+                <p>
+                    The event will kick off with regional marchers and parades arriving at 3pm, followed by a shared
+                    picnic with live entertainment and a 'democratic marketplace' of discussions, sharing of ideas and
+                    debate. This will act as a first 'health check' of our democratic practices and devising practical
+                    reforms to recondition and redesign a democracy that works for us all in the 21st century.                </p>
+                <p>
+                    For more information about the 17th August Illegal Picnic event, including opportunities for stall
+                    holders, see our <a href="http://peterloomassacre.org/news.html" target="_blank">events</a> page.
+                </p>
+                <p>
+                    We will open the Six Acts online poll website at 4pm, 17th August.
+                    This is when you will be able to log in, submit proposals and begin voting on them.
+                    <i>Remember: Each Six Acts proposal will be strictly limited to 100 words maximum.</i>
+                </p>
+                <h2 class="is-3">The Launch</h2>
+                <p>
+                    We are not <i>limiting the number of proposals</i> any individual or organisation can submit to the
+                    poll, or the number of proposals an individual or organisation can vote for.
+                </p>
+                <p>
+                    A user can <i>only vote for each proposal</i> once and this vote <i>can be removed</i> by the user
+                    at any time  during the live poll period, should they change their minds. You can also delete
+                    your proposal at any time if you feel it has been superseded by another, improved one.
+                </p>
+                <p>
+                    While we leave it to the user to decide the number of proposals they vote for, we urge people to
+                    treat their votes sparingly, with respect, and only vote for proposals that you believe are genuine,
+                    effective and feasible reforms for optimising our democratic practices.
+                </p>
+                <p>
+                    A user can submit a new proposal to the poll at any time during the live period. There are some
+                    practical restrictions and guidelines for submissions which we have implemented to prevent
+                    misuse and maximise constructive input. Please read our
+                    <a href="{{Site::menuLink('nine_rules')}}">9 Rules for the 6 Acts</a> and
+                    <a href="{{Site::menuLink('categories')}}">Categories Explained</a> documents before
+                    submitting a proposal.
+                </p>
+                <p>
+                    We urge participants to road-test their proposals on friends, colleagues and the Six Acts
+                    community social media platforms before submitting to the poll. This will give you a chance to
+                    refine, tweak or reconsider your proposal, giving it a better chance of being a contender for the
+                    final Six Acts document. It is also important that you are completely happy with your proposal at
+                    the time you submit it because <i>once submitted you will not be able to edit it.</i>
+                    (Only delete and replace it.)
+                </p>
+                <h2 class="is-3">The Result</h2>
+                <p>
+                    The online poll will be live from the <b>17th August.</b>
+                    During the live period we will be taking <b>regular snapshots</b> of the current version of the
+                    document. <b>The first of these will be published shortly before the Conservative government's
+                    annual party conference, which will be held in September at the site of the Peterloo Massacre.</b>
+                </p>
+                <p>
                     <i>
-                        The Six Acts project is an online platform to crowd-source radical ideas for
-                        improving and redesigning the way we practice democracy in the 21st century.                    </i>
-                </h2>
-                <p>
-                    The poll will open for submissions and voting at 4pm on the 17th August 2019.
+                        An appropriate final closing date for the finished Six Acts document will be collectively chosen later.
+                    </i>
                 </p>
                 <p>
-                    We look forward to your participation. In the meantime, please familiarise yourself with the
-                    information on this page which explains how the poll will be conducted and help you ensure that
-                    your proposals are submitted correctly.
+                    Users can also get a <i>Status Report</i> of the top-ranking proposals at any time. This report
+                    will be a summary of the top proposals which are leading in each of the six reform categories.
                 </p>
                 <p>
-                    Get thinking, get your acts together, and we'll see you back here on the 17th August!
+                    The Six Acts team will also announce and display status reports at regular intervals, which will
+                    include not only the leading proposal in each category, but will also have links to the other 5
+                    closest contenders.
+                </p>
+                <h2 class="is-3">The Result</h2>
+                <p>
+                    The end document will be a single-page mandate from the people which articulates their demands
+                    for democratic reform. We invite the media, campaign groups, organisations, individuals and
+                    politicians to cite, publicise and adopt these Six Acts as a basis for policy-making or further
+                    research.
+                </p>
+                <p>
+                    The Six Acts document will also serve as a position statement or manifesto that groups and
+                    individuals can sign up to and endorse, as well as being adapted into a petition to be presented to
+                    the Petitions Committee for debate in Parliament.
+                </p>
+                <p>
+                    All of the proposals submitted to the poll will be preserved on the site in an archive for future
+                    debate and reference.
+                </p>
+                <p>
+                    <i>
+                        Rather than a fixed, rigid manifesto, we want the Six Acts to be a working document which can
+                        be updated or reviewed annually and revisited on an ongoing basis... particularly on the 16th
+                        of August each year
+                    </i>
                 </p>
             </div>
         </div>
