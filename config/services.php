26e607b4511888807646b5f9e3d5bd23ea78bb79
Petros Diveris <petros@urban-eye.com>
2019-07-30 01:29:30 +0100

    This correctly sets the callback URL for the social media login handlers to the right host - that is, the host as set in .env file, for security reasons.
    Closed #64.

--- config/services.php
+++ config/services.php
@@ -46,17 +46,17 @@ return [
     'twitter' => [
         'client_id' => env('TWITTER_CLIENT_ID'),
         'client_secret' => env('TWITTER_CLIENT_SECRET'),
-        'redirect' => 'https://beta.sixacts.org/login/twitter/callback',
+        'redirect' => env('APP_URL').'/login/twitter/callback',
     ],
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'https://beta.sixacts.org/login/google/callback',
+        'redirect' => env('APP_URL').'/login/google/callback',
     ],
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' => 'https://beta.sixacts.org/login/facebook/callback',
+        'redirect' => env('APP_URL').'/login/facebook/callback',
     ],
     
 ];
