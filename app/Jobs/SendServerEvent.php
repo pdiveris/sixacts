9e3cc0c5b30939f84509529dcfe315ee0fe5ae8c
Petros Diveris <petros@urban-eye.com>
2019-08-04 20:58:46 +0100

    Worked issue #98. Successfully dispatching and receiving messages with nchan.
    Closed #98.

---
+++ app/Jobs/SendServerEvent.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use GuzzleHttp\Client;
+
+class SendServerEvent implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $payload;
+    
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($payload)
+    {
+        $this->payload = $payload;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function handle()
+    {
+        \App\Http\Controllers\ServerSideEventsController::fire();
+    }
+}
